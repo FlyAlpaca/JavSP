40d4f1c1b0100a41c6d0c8484168efbc45433e4f
Xu <dark.soaper@gmail.com>
2026-06-07 15:11:50 +0800

    refactor: 移除 arzon/arzon_iv 爬虫源
    arzon 网站已不可用，移除相关爬虫代码和测试数据。

--- javsp/web/arzon_iv.py
+++
@@ -1,9 +0,0 @@
-"""从arzon抓取Image Video数据（委托给arzon模块）"""
-
-from javsp.datatype import MovieInfo
-from javsp.web.arzon import parse_data as _parse_data
-
-
-def parse_data(movie: MovieInfo):
-    """解析指定番号的Image Video影片数据"""
-    _parse_data(movie, mode="arzon_iv")
